e64a06904d1d4dd411bf4abb5a42e4432575492d
Felix Giese <s4fegies@uni-trier.de>
2019-08-25 23:15:58 +0200

    last state found on disk

--- constants.h
+++ constants.h
@@ -3,7 +3,7 @@
 #include <limits.h>
 #include <sys/types.h>
 
-#define MAX_CONNECTIONS 256
+#define MAX_CONNECTIONS 5
 #define MAX_SOCKETS (MAX_CONNECTIONS + 2)
 #define HEADER_SIZE 3
 #define MAX_PAYLOAD_SIZE (PIPE_BUF - HEADER_SIZE)
